8bd1edcbd0aa7bf906cd398170e31bdf9acd4371
lxykad <503215875@qq.com>
2016-10-07 22:53:09 +0800

    add rx

--- app/src/main/java/com/lxy/retrofit/rx/RxActivity.java
+++ app/src/main/java/com/lxy/retrofit/rx/RxActivity.java
@@ -9,44 +9,77 @@ import android.view.View;
 import com.lxy.retrofit.BR;
 import com.lxy.retrofit.R;
 
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
 import rx.Observable;
 import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 
 public class RxActivity extends AppCompatActivity {
 
     private ViewDataBinding mBinding;
+    private String mBaseUrl = "http://gank.io/api/data/Android/";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_rx);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_rx);
-        mBinding.setVariable(BR.presenter,new Presenter());
+        mBinding.setVariable(BR.presenter, new Presenter());
 
 
+        loadData();
+    }
 
-
+    public interface RxService {
+        @GET("10/1")
+        Observable<RxBean> loadData();
 
     }
 
 
+    public class Presenter {
 
+        public void onClick(View view) {
 
-    public class Presenter{
-
-        public void onClick(View view){
-            //Observable释放Hello，World字符串，接着停止执行：
-            Observable.OnSubscribe onSubscribe = new Observable.OnSubscribe<String>(){
-                @Override
-                public void call(Subscriber<? super String> subscriber) {
-                    subscriber.onNext("nihao");
-                    subscriber.onCompleted();
-                }
-            };
-            Observable observable = Observable.create(onSubscribe);
         }
     }
 
+    public void loadData() {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(mBaseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build();
+        RxService rxService = retrofit.create(RxService.class);
+        rxService.loadData()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<RxBean>() {
+                    @Override
+                    public void onCompleted() {
+                        System.out.println("0000000======onCompleted");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        System.out.println("0000000======onError==="+e.toString());
+                    }
+
+                    @Override
+                    public void onNext(RxBean bean) {
+                        System.out.println("0000000======onNext==="+bean.getResults().size());
+                    }
+                });
+
+    }
 
 }
 
