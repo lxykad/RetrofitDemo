2d7b4f55fe3249acb716f0cae5ceb4b9cb8ae5ac
lxy <C@lxydeMac-mini.local>
2016-10-12 15:12:12 +0800

    add testJava

---
+++ testjava/src/main/java/com/lxy/java/MyClass.java
@@ -0,0 +1,10 @@
+package com.lxy.java;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+        int a = 3;
+        a += a -= a *= a += a;
+        System.out.println("6666666======aaaaa=====" + a);
+    }
+}
